ed97c0fff1e8505185f0e5df1254e3ec4671fc5b
TMoechel <t.moechel@softcreation.de>
2019-02-21 10:51:40 +0100

    added directories

---
+++ Fs.TestMapping/NHibernateHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NHibernate;
+using NHibernate.Cfg;
+
+namespace FR.WebApi.InfraStructure
+{
+    public sealed class NHibernateHelper
+    {
+       private static readonly ISessionFactory _sessionFactory;
+
+        static NHibernateHelper()
+        {
+            _sessionFactory = new Configuration().Configure().BuildSessionFactory();
+        }
+
+        public static ISession GetSession()
+        {
+            var currentSession = _sessionFactory.OpenSession();
+            return currentSession;
+        }
+
+        public static void CloseSessionFactory()
+        {
+            if (_sessionFactory != null)
+            {
+                _sessionFactory.Close();
+            }
+        }
+    }
+}
---
+++ Fs.TestMapping/OrderTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using Fs.Dto;
+
+namespace Fs.TestMappin
+{
+   [TestFixture]
+    public class OrderTests
+    {
+        [Test]
+        public void TestCreateOrder()
+        {
+            DtoOrder order = new DtoOrder();
+        }
+    }
+}
